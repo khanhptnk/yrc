9f4070d4cea8157e2875f57f9c977ea4ef2d204e
Khanh Nguyen <nguyenxuankhanhm@gmail.com>
2025-05-27 02:33:56 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 project = 'YRC-Bench'
 copyright = '2025, Mohamad H. Danesh, Tu Trinh, Benjamin Plaut, Nguyen X. Khanh'
 author = 'Mohamad H. Danesh, Tu Trinh, Benjamin Plaut, Nguyen X. Khanh'
