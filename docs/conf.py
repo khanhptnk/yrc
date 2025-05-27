3b8f115e4b5063d6f8e0fde0be344d7c8f5ed5e2
Khanh Nguyen <nguyenxuankhanhm@gmail.com>
2025-05-27 02:26:02 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,12 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',   # for Google-style or NumPy-style docstrings
+    'sphinx.ext.viewcode',   # optional: links to source code
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
