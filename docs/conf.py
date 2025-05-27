d833fc202d860e3af0073a0582f5066a703e2eb8
Khanh Nguyen <nguyenxuankhanhm@gmail.com>
2025-05-27 02:35:24 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,14 +24,24 @@ extensions = [
     'sphinx.ext.viewcode',   # optional: links to source code
 ]
 
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+autosummary_generate = True
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
