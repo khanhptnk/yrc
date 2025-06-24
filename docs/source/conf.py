f7590d8f5357f09b6f6762fe6ae826d4ddb31ffa
Khanh Nguyen <kxnguyen@rnn.ist.berkeley.edu>
2025-06-24 13:51:34 -0700

    readthedocs sphinx

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'YRC'
+copyright = '2025, Khanh Nguyen, Mohamad Danesh, Alina Trinh, Ben Plaut'
+author = 'Khanh Nguyen, Mohamad Danesh, Alina Trinh, Ben Plaut'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
